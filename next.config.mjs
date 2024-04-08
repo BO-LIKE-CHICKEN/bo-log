40e1240f678414edd4fdd4dce6f0e76ad3597263
bosung baek <teabs1125@gmail.com>
2024-04-08 14:45:26 +0900

    chore: vanila extract 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,14 @@
-import nextMDX from "@next/mdx";
+import nextMDX from '@next/mdx';
+
+import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin';
+
+const withVanillaExtract = createVanillaExtractPlugin();
 
 const withMDX = nextMDX();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
 };
 
-export default withMDX(nextConfig);
+export default withVanillaExtract(withMDX(nextConfig));
